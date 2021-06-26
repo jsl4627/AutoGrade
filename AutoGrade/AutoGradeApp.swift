b8d747309d19ddc47b71b522f7daed30a109bc5a
jared-lantner <jlantner123@icloud.com>
2021-06-26 13:18:14 -0400

    Initial Commit

---
+++ AutoGrade/AutoGradeApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoGradeApp.swift
+//  AutoGrade
+//
+//  Created by Jared Lantner on 6/26/21.
+//
+
+import SwiftUI
+
+@main
+struct AutoGradeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
